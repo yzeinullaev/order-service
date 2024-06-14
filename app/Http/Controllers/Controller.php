897492f3c15f051a52f6239668bf82bfc32e8a32
yzeinullayev <yzeinullaev@gmail.com>
2024-06-14 19:28:01 +0500

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="0.0.1",
+ *      title="Order Service (API)",
+ *      description="<b>Описание:</b>",
+ * )
+ *
+ * @OA\Tag(
+ *     name="Orders",
+ *     description="API Endpoints of Orders"
+ * )
+ * @OA\SecurityScheme(
+ *   type="http",
+ *   description="Authorization token",
+ *   scheme="bearer",
+ *   bearerFormat="JWT",
+ *   securityScheme="bearerAuth",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests, ResponseTrait;
